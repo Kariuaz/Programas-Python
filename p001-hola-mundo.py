9a436f0ae840881f07149c31e6cc4d5dfec5e75e
Kariuaz <35165728@uaz.edu.mx>
2026-02-05 13:49:38 -0600

    Actividad 3

--- p001-hola-mundo.py
+++ p001-hola-mundo.py
@@ -1,1 +1,14 @@
 # p001-hola-mundo.py
+# Lee datos y envia un saludo
+
+print("Leyendo datos y enviando un saludo: \n")
+
+
+nombre = input("Dame tu nombre: ")
+edad = input("Dame tu edad: ")
+peso = input("Dame tu peso: ")
+
+print(f"{nombre} bienvenido a Python, tu edad es {edad} y tu peso es {peso}")
+print(type(nombre))
+print(type(edad))
+print(type(peso))
