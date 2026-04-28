3eac223992aed913147b4364cd881bae24305d78
Kariuaz <35165728@uaz.edu.mx>
2026-04-27 22:49:40 -0600

    Actividad 17

---
+++ p127-funcion-parametro.py
@@ -0,0 +1,8 @@
+# p127-funciones-parametros.py 
+
+def saluda(nombre:str)-> None:
+    print(f'Hola {nombre} bienvenido a Python, tu nombre tiene {len(nombre)} caracteres')
+
+saluda ('Kari')
+saluda ('MITA')
+saluda ('Hetor')
